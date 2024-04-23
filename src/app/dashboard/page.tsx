756ade59f47711826460c888c97534ebca0a83c8
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-04-24 02:27:46 +0600

    setup project also routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            dashboard home
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/dashboard/products/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            manage products
+        </div>
+    );
+};
+
+export default page;
