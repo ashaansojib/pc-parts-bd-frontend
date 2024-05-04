aebe8c3f07c849aa843a55f6e99879d79b69a4b8
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-05-04 13:38:35 +0600

    show product table on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,68 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { DashboardTitle } from "./shared/DashboardTitle";
-import { SubmitHandler, useForm } from "react-hook-form";
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { FaEdit } from "react-icons/fa";
+import { FaDeleteLeft } from "react-icons/fa6";
+interface ProductsProps {
+  id: string;
+  name: string;
+  image: string;
+  price: string;
+}
 const page = () => {
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+  useEffect(() => {
+    fetch("/fake/case.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  // .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
 
   return (
     <div>
       <DashboardTitle title="All Products Manage Here!" />
       <div>
-        <div>
-            all products
-        </div>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell>SL</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Category</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, i) => (
+              <TableRow key={product.id}>
+                <TableCell>{i + 1}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>PC Case</TableCell>
+                <TableCell className="flex gap-2 items-center">
+                    <FaEdit className="text-xl cursor-pointer" />
+                    <FaDeleteLeft className="text-xl cursor-pointer" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {/* <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      /> */}
       </div>
     </div>
   );
